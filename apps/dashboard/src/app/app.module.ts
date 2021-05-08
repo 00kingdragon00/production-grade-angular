cef30db8e5ff24219a9424c11e37168beb12bd0e
king-dragon <utkarshchauhan17@outlook.com>
2021-05-08 17:49:51 +0530

    declare components

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { CoreDataModule, CoreDataRoutes } from '@pga/core-data';
+import { CoreDataModule, coreDataRoutes } from '@pga/core-data';
 import { CoreStateModule, coreStateRoutes } from '@pga/core-state';
 import { MaterialModule, materialRoutes } from '@pga/material';
 import { RoutingModule } from './routing.module';
 import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsDetailsComponent } from './widgets/widgets-details/widgets-details.component';
 
 @NgModule({
-  declarations: [AppComponent, WidgetsComponent],
+  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetsDetailsComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
--- libs/core-data/src/lib/core-data.module.ts
+++ libs/core-data/src/lib/core-data.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 
-export const CoreDataRoutes: Route[] = [];
+export const coreDataRoutes: Route[] = [];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
