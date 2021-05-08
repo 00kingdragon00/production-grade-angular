1ef628cfe13e6d814d0bd7aab1e720329a0befc7
king-dragon <utkarshchauhan17@outlook.com>
2021-05-08 21:34:08 +0530

    create widget details ui

--- apps/dashboard/src/app/widgets/widgets-details/widgets-details.component.ts
+++ apps/dashboard/src/app/widgets/widgets-details/widgets-details.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Widget } from '@pga/api-interfaces';
 @Component({
   selector: 'pga-widgets-details',
   templateUrl: './widgets-details.component.html',
   styleUrls: ['./widgets-details.component.scss']
 })
-export class WidgetsDetailsComponent implements OnInit {
+export class WidgetsDetailsComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  currentWidget: Widget;
+  originalTitle = '';
+  @Input() set widget(value: Widget) {
+    if(value) this.originalTitle = value.title;
+    this.currentWidget = {...value};
+  };
+  @Output() saved = new EventEmitter;
+  @Output() cancelled = new EventEmitter;
 }
