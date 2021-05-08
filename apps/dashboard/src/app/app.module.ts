10cd3fb088df8274158f01d4260a3f739e08dd0a
king-dragon <utkarshchauhan17@outlook.com>
2021-05-08 17:39:45 +0530

    generate(lib): core data

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { CoreDataModule, CoreDataRoutes } from '@pga/core-data';
+import { CoreStateModule, coreStateRoutes } from '@pga/core-state';
+import { MaterialModule, materialRoutes } from '@pga/material';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
