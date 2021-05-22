5b84b9e1738afafe05b307163aae8d583162d3fc
king-dragon <utkarshchauhan17@outlook.com>
2021-05-22 19:54:53 +0530

    add: logic to show widgets list in client app

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Widget } from '@pga/api-interfaces';
+import { WidgetsService } from '@pga/core-data';
 @Component({
   selector: 'pga-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  widgets$: Observable<Widget[]>;
+
+  constructor(private widgetsService: WidgetsService) {}
+
+  ngOnInit() {
+    this.widgets$ = this.widgetsService.all();
+  }
 }
