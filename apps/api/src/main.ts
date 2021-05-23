b0645187e2c7c2d049813ae7bd08e5524fc8219c
king-dragon <utkarshchauhan17@outlook.com>
2021-05-23 19:37:46 +0530

    add: swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,27 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const configureSwagger = (app) => {
+  const options = new DocumentBuilder()
+    .setTitle('Production Grade Angular API')
+    .setDescription('REST API for the Production Grade Angular App')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.enableCors();
   app.setGlobalPrefix(globalPrefix);
+
+  configureSwagger(app);
+
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
